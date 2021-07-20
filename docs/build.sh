f2eb73943c11f35329c5f7f75fa6cd467ea3c7e7
richard <github@fantastic.me.uk>
2021-07-20 01:51:52 +0100

    switch from MD to RST

--- docs/build.sh
+++ docs/build.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-pandoc tutorial.md --filter pandoc-emphasize-code -o tutorial.pdf --from markdown --number-sections --top-level-division=chapter --toc --syntax-definition gd-script.xml --highlight-style=tango  --template eisvogel --pdf-engine xelatex
-pandoc tutorial.md --filter pandoc-emphasize-code -o tutorial.html --from markdown --number-sections --top-level-division=chapter --toc --syntax-definition gd-script.xml --highlight-style=tango  --template template.html
+pandoc tutorial.rst --filter pandoc-emphasize-code -o tutorial.pdf --from rst --number-sections  --toc --syntax-definition gd-script.xml --highlight-style=tango  --metadata-file=meta.yaml --template eisvogel --pdf-engine xelatex --top-level-division=chapter
+pandoc tutorial.rst --filter pandoc-emphasize-code -o tutorial.html --from rst --number-sections --top-level-division=chapter --toc --syntax-definition gd-script.xml --metadata-file=meta.yaml --highlight-style=tango  --template template.html
